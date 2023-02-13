29578b6b68659e661b048f4f860c76590d7d0cd1
Ananmay Sharan <32856566+ananmaysharan@users.noreply.github.com>
2023-02-13 17:22:40 -0500

    added styling

--- script.js
+++ script.js
@@ -12,6 +12,26 @@ const map = new mapboxgl.Map({
 });
 
 map.on('load', () => {
+
+    map.addSource('cycling', {
+        type: 'geojson',
+        // Use a URL for the value for the `data` property.
+        data: 'https://ananmaysharan.github.io/ggr472-lab2/cycling-network.geojson'
+        });
+
+        map.addLayer({
+            'id': 'cyclinglines',
+            'type': 'line',
+            'source': 'cycling', //must match source ID from addSource method
+            'paint': {
+                'line-color': '#b42222',
+                'line-width': 3,
+                'line-opacity': 0.4
+            }
+        },
+        );
+
+
     map.addSource('bikeshare', {
         type: 'geojson',
         data: 'https://ananmaysharan.github.io/ggr472-lab2/bikeshare.geojson'
@@ -27,20 +47,39 @@ map.on('load', () => {
     }
     );
 
-    map.addSource('cycling', {
-        type: 'geojson',
-        // Use a URL for the value for the `data` property.
-        data: 'https://ananmaysharan.github.io/ggr472-lab2/cycling-network.geojson'
-        });
+    // When a click event occurs on a feature in the places layer, open a popup at the
+// location of the feature, with description HTML from its properties.
+map.on('click', 'bikesharenetwork', (e) => {
+    // Copy coordinates array.
+    const coordinates = e.features[0].geometry.coordinates.slice();
+    const station = e.features[0].properties.station;
+    const docks = e.features[0].properties.Total_docks;
 
-        map.addLayer({
-            'id': 'cyclinglines',
-            'type': 'line',
-            'source': 'cycling', //must match source ID from addSource method
-            'paint': {
-                'line-color': '#888',
-                'line-width': 8
-            }
-        },
-        );
+     
+    // Ensure that if the map is zoomed out such that multiple
+    // copies of the feature are visible, the popup appears
+    // over the copy being pointed to.
+    while (Math.abs(e.lngLat.lng - coordinates[0]) > 180) {
+    coordinates[0] += e.lngLat.lng > coordinates[0] ? 360 : -360;
+    }
+     
+    new mapboxgl.Popup()
+    .setLngLat(coordinates)
+    .setHTML('<p>' + "Station Name: " + station + '<br>' + "Bike Docks: " + docks + '</p>')
+    .addTo(map);
+    });
+     
+    // Change the cursor to a pointer when the mouse is over the places layer.
+    map.on('mouseenter', 'places', () => {
+    map.getCanvas().style.cursor = 'pointer';
+    });
+     
+    // Change it back to a pointer when it leaves.
+    map.on('mouseleave', 'places', () => {
+    map.getCanvas().style.cursor = '';
+    });
+
+    const stateLegendEl = document.getElementById('state-legend');
+
+    
 });
